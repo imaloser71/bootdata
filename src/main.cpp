37626c86b4bcd33a0ac2651f41f0b9b0e5aca6a4
imaloser71 <102986295+imaloser71@users.noreply.github.com>
2022-05-25 15:42:31 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -49,8 +49,27 @@ void visualizeBuffer(char* buffer) {
     SetConsoleTextAttribute(hConsole, 15);
     _tprintf(TEXT("\n\n"));
 }
-int main() {
+int main(int argc, char** argv) {
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    int paramFlag = 0;
+
+    if (argc > 1) {
+        if (argc == 1) {
+            SetConsoleTextAttribute(hConsole, 12);
+            _tprintf(TEXT("Invalid Argument Count.\n"));
+        }
+        else if (samestring(argv[1], "export")) {
+            paramFlag = 1;
+        }
+        else if (samestring(argv[1],"import")) {
+            paramFlag = 2;
+        }
+        else {
+            SetConsoleTextAttribute(hConsole, 12);
+            _tprintf(TEXT("Invalid Arguments, Check documentation.\n"));
+        }
+    }
 
     SetConsoleTextAttribute(hConsole, 6);
     _tprintf(TEXT("Bootloader data in \"\\\\.\\PhysicalDrive0\"\n\n"));
@@ -58,18 +77,81 @@ int main() {
     DWORD dwBytesToRead = 512;
     DWORD dwBytesRead = 0;
     BOOL bErrorFlag = FALSE;
-    HANDLE h = CreateFile(L"\\\\.\\PhysicalDrive0", GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+
+    if (paramFlag == 2) {
+        SetConsoleTextAttribute(hConsole, 4);
+        std::string answer;
+        std::cout << "\nAre you SURE your filepath is correct? IT HAS TO BE AN ABSOLUTE PATH, NOT A RELATIVE ONE, OR ELSE IT WILL BREAK YOUR BOOTLOADER! (y/n)";
+        std::cin >> answer;
+        if (answer != "y") {
+            SetConsoleTextAttribute(hConsole, 15);
+            return -1;
+        }
+        answer = "";
+        SetConsoleTextAttribute(hConsole, 12);
+        std::cout << "\nAre you ABSOLUTELY SURE you want to PERMANENTLY OVERWRITE your bootloader? Make SURE you have BACKUPS! (y/n)";
+        std::cin >> answer;
+        if (answer != "y") {
+            SetConsoleTextAttribute(hConsole, 15);
+            return -1;
+        }
+        h = CreateFile(L"\\\\.\\PhysicalDrive0", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+        std::string istr = std::string(512, '-');
+        std::ifstream ifs(argv[2], std::ios_base::binary);
+        ifs >> istr;
+        for (int i = 0; i < 512; i++) {
+            DataBuffer[i] = istr[i];
+        }
+        bErrorFlag = WriteFile(h, DataBuffer, 512, &dwBytesRead, NULL);
+        if (bErrorFlag) {
+            SetConsoleTextAttribute(hConsole, 15);
+            _tprintf(TEXT("\nSuccess! %d bytes written\n"), dwBytesRead);
+        }
+        else {
+            SetConsoleTextAttribute(hConsole, 12);
+            _tprintf(TEXT("ReadFile returned 0.\nGetLastError returned %d.\n"), GetLastError());
+        }
+    }
+    else {
+        h = CreateFile(L"\\\\.\\PhysicalDrive0", GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+    }
     bErrorFlag = ReadFile(h, DataBuffer, 512, &dwBytesRead, NULL);
     if (bErrorFlag) {
+        if (paramFlag == 1) {
+            std::string ostr = std::string(512, '-');
+            for (int i = 0; i < 512; i++) {
+                ostr[i] = DataBuffer[i];
+            }
+            std::ofstream ofs(argv[2], std::ios_base::binary);
+            if (!ofs) {
+                SetConsoleTextAttribute(hConsole, 12);
+                _tprintf(TEXT("Cannot open '%s', %s"), argv[2], strerror(errno));
+            }
+            else {
+                ofs << ostr;
+                ofs.close();
+                SetConsoleTextAttribute(hConsole, 15);
+                std::cout << "Successfully exported bootloader to '" << argv[2] << "'";
+            }
+            CloseHandle(h);
+            SetConsoleTextAttribute(hConsole, 15);
+            return 1;
+        }
         SetConsoleTextAttribute(hConsole, 15);
         _tprintf(TEXT("ANSI:\n"));
         visualizeBuffer(((DataBuffer)));
-        _tprintf(TEXT("Success! %d bytes read\n"), dwBytesRead);
+		if(paramFlag == 2){
+			_tprintf(TEXT("If the result above is not equivelant to your boot file, it is possible it was not imported correctly, in such case please prepare for unexpected results.\n"), dwBytesRead);
+		}else{
+			_tprintf(TEXT("Success! %d bytes read\n"), dwBytesRead);
+		}
     }
     else {
         SetConsoleTextAttribute(hConsole, 12);
         _tprintf(TEXT("ReadFile returned 0.\nGetLastError returned %d.\n"),GetLastError());
     }
     CloseHandle(h);
-    return 0;
+    SetConsoleTextAttribute(hConsole, 15);
+    return -1;
 }
